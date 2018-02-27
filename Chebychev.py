14bc3af348c243f4d26b7bb386f45d01d5ea2cd6
ArhumUchiha <arhum.ahmad@mail.utoronto.ca>
2018-02-27 15:32:23 -0500

    added Chebychev's Theorem

--- Chebychev.py
+++ Chebychev.py
@@ -4,6 +4,25 @@ def getK(num, mean, sd):
 
     return K
 
-def LowerBound(num, mean, sd):
+def Within(num, mean, sd):
     K = getK(num, mean, sd)
     return 1 - (1/(K**2))
+
+def PLess(num, mean, sd):
+    Pwithin = Within(num, mean, sd)
+    if num >= mean:
+        return (Pwithin/2) + 0.5
+    else:
+        return 1 - (Pwithin/2)
+
+def PMore(num, mean, sd):
+    return 1 - PLess(num, mean, sd)
+
+def PBetween(num1, num2, mean, sd):
+    if num1 > num2:
+        swap = num1
+        num1 = num2
+        num2 = swap
+    P1 = PLess(num1, mean, sd)
+    P2 = PLess(num2, mean, sd)
+    return P2 - P1
