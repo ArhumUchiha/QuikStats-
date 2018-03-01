9b260592f64c0aa6411cdf729c39a4831a6dc7ae
ArhumUchiha <arhum.ahmad@mail.utoronto.ca>
2018-03-01 01:15:41 -0500

    added confidence interval finder

---
+++ Confidence.py
@@ -0,0 +1,11 @@
+import scipy.stats as stats
+import math
+import numpy
+
+def CI(smean, sd, confidence, Samples):
+    alpha = 1 - confidence
+    zstar = abs( stats.norm.ppf(alpha/2) )
+    MarginOfError = zstar * (sd / math.sqrt(Samples))
+    LowerBound = smean - MarginOfError
+    UpperBound = smean + MarginOfError
+    return str(LowerBound) + " " + str(UpperBound)
